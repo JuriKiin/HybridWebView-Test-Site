cf408c418f39d5e8e0b6b59a71cfe1701405ccf2
Jüri Kiin <kiin.juri@gmail.com>
2023-05-10 16:58:08 -0400

    Updated JS to add a forced error state
    Updated html to finalize buttons

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 let bridge = {
     testWebImplementation: (userId) => {
-        registerUserForPushNotifications(userId, (error, result) => {
-            if (error) {
-              window.hybrid.showToast(error);
-            } else {
-              window.hybrid.showToast(result || '');
-            }
+        registerUserForPushNotifications(userId, (result) => {
+            let message = result != null ? result : "Error"
+            window.hybrid.showToast(message)
         });
     },
-    testAsync: () => {
-        window.hybrid.testAsync("Message from web", 'bridge.testSync');
+    testAsyncError: (userId) => {
+        asyncError(userId, (result) => {
+            let message = result != null ? result : "Error"
+            window.hybrid.showToast(message)
+        });
     },
     testSync: () => {
         window.hybrid.showToast("Hello World!");
     },
-    testCallback: (message) => {
-        window.hybrid.showToast(message);
-    }
 }
 
 window.registerUserForPushNotifications = (userId, callback) => {
     //if using web, web provides implementation here
     //if using mobile, modile will override/inject implementation
-    window.hybrid.showToast("This will be overwritten")
+    console.log("If you see this, we are not on mobile.")
+}
+
+window.asyncError = (userId, callback) => {
+    console.log("If you see this, we are not on mobile.");
 }

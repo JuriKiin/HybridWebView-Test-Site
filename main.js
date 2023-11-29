4501d43a67245dc242eb1997ea38b56ba887b920
Jüri Kiin <kiin.juri@gmail.com>
2023-11-29 13:28:20 -0500

    Updates for easier development testing
    Updated UI
    Added more generic function names
    removed bridge as no longer needed
    Added customization settings for realtime testing

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-let bridge = {
-    removeDOM: () => {
-        document.querySelector('.loader').remove();
-    },
-    testWebImplementation: (userId) => {
-        registerUserForPushNotifications(userId, (result) => {
-            let message = result != null ? result : "Error"
-            window.sampleName.showToast(message)
-        });
-    },
-    testAsyncError: (userId) => {
-        asyncError(userId, (result) => {
-            let message = result != null ? result : "Error"
-            window.sampleName.showToast(message)
-        });
-    },
-    testSync: () => {
-        window.sampleName.showToast("Hello World!");
-    }
+let endpointName = "endpoint";
+
+//Web functions
+let callAsync = (userId) => {
+    userId(userId, (result) => {
+        let message = result != null ? result : "Error"
+        window[endpointName].showToast(message)
+    });
 }
 
-window.registerUserForPushNotifications = (userId, callback) => {
+let callAsyncError = (userId) => {
+    asyncError(userId, (result) => {
+        let message = result != null ? result : "Error"
+        window[endpointName].showToast(message)
+    });
+}
+
+let callSync = () => {
+    window[endpointName].showToast("Hello World!");
+}
+
+let removeDOM = () => {
+    document.querySelector('.loader').remove();
+}
+
+//Customization Functions
+
+function updateEndpointName() {
+    endpointName = document.getElementById("endpoint").value;
+    console.log(endpointName);
+}
+
+function loadUrl() {
+    window.location.href = document.getElementById('url').value;
+}
+
+
+//Injected JS functions
+
+window.async = (userId, callback) => {
     //if using web, web provides implementation here
     //if using mobile, modile will override/inject implementation
     console.log("If you see this, we are not on mobile.")
